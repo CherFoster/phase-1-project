ecc61f0b66df4b95d1e0855083380bde2bbf874c
Cherise Foster <cherise.foster@outlook.com>
2023-04-01 10:59:23 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.querySelector('form')
-    const searchBar = document.getElementById('searchbar')
-    let divCollect = document.querySelector('#database')
+    const searchBar = document.querySelector('#searchbar')
+    let cardCollection = document.querySelector('#database')
     const footer = document.querySelector('footer')
+    const EMPTY_HEART = '♡';
+    const FULL_HEART = '♥';
+
+    
 
 // Renders each card to page
 function renderCards() {
@@ -28,40 +32,66 @@ function renderCards() {
     
        btn.setAttribute('id', `${workout.id}`)
        btn.classList.add('save-btn')
-       btn.innerText = "save";
+       btn.innerText = "♡";
     
     
     // Append elements to card
-      const divCard = document.createElement('div')
-      divCard.classList.add('card')
-      divCard.append(h4, img, btn)
-      divCollect.append(divCard)
+      const workoutCard = document.createElement('div')
+      workoutCard.classList.add('card')
+      workoutCard.append(h4, img, btn)
+      cardCollection.append(workoutCard)
     
       
-      divCard.addEventListener("mouseover", () => divCard.setAttribute("style", "box-shadow: 30px 30px 10px #545E63"));
-      divCard.addEventListener("mouseout", () => divCard.setAttribute("style", "box-shadow: 0px 0px"));
-    
-        };
+      workoutCard.addEventListener("mouseover", () => workoutCard.setAttribute("style", "box-shadow: 30px 30px 10px #545E63"));
+      workoutCard.addEventListener("mouseout", () => workoutCard.setAttribute("style", "box-shadow: 0px 0px"));
 
+    //   workoutCard.addEventListener('click', getWorkoutId)
+    
+    //   Saves workout when save button is clicked
+    btn.addEventListener('click', (event) => {
+        if (event.target.classList[0] === 'save-btn') {
+            const activated = event.target.classList.contains('activated-heart');
+            if(activated) {
+                event.target.classList.remove('activated-heart')
+                event.target.innerText = EMPTY_HEART
+            }else {
+                event.target.classList.add("activated-heart");
+                event.target.innerHTML = FULL_HEART;
+            }
+        }
+    })
+    
+};
+
+// function getWorkoutId(){
+//     fetch(`http://localhost:3000/workouts/${workouts.id}`)
+//     .then(resp => resp.json())
+//     .then(createCard)
+// }
+        
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 
 document.querySelectorAll('.card').forEach(event => event.remove())
-document.querySelectorAll('.not-found').forEach(event => event.remove())
 
 
-fetch('http://localhost:3000/workouts')
-  .then(resp => resp.json())
-  .then(workouts => {
-    workouts.forEach(workout => {
-        if(workout.muscle === true){
-        createCard(workout)
-    }})
+  });
 
-    handleError()},)
-    .catch(error => console.error(error))
-  })
+//   Returns workouts for selected muscle
+// const selectedMuscle = searchBar.value
+// function muscleSelection(){
+//     fetch('http://localhost:3000/workouts')
+//     .then(resp => resp.json())
+//     .then(workouts => {
+//         workouts.forEach(workout => {
+//             if(workout.muscle === selectedMuscle){
+//                 createCard(workout)
+//             }
+//         })
+//     })
+//     }
+//     muscleSelection();
 
   
 // Creates reset button
@@ -73,10 +103,25 @@ footer.appendChild(resetBtn)
 resetBtn.addEventListener('click', (event) => {
     event.preventDefault();
     renderCards();
-    document.querySelector('.not-found').forEach(event => event.remove())
+    document.querySelectorAll('.card').forEach(event => event.remove())
   })
 };
-resetButton()
+resetButton();
 
+// Creates a Like button
+function likesButton(){
+    const likesBtn = document.createElement('button')
+    likesBtn.classList.add('liked-workouts')
+    likesBtn.innerText = "Liked"
+    footer.appendChild(likesBtn)
+
+    likesBtn.addEventListener('click', (event) => {
+        // event.preventDefault();
+        // if the heart is activated, save card
+        // console.log(event)
+  
+    })
+}
+likesButton()
 
 })
