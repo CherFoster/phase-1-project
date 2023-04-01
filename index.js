60f7e625b5afc01ccfcd2f77efaab1a174f2b585
Cherise Foster <cherise.foster@outlook.com>
2023-03-31 19:56:02 -0600

    updated index.js and css

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const workoutData = document.getElementById('database')
-    const sumbitButton = document.querySelector('.search')
     const form = document.querySelector('form')
-    const workoutImg = document.createElement('img')
-    
-    // Create a span of the workouts on the main page
-    function displayWorkout(workouts){
-        workouts.forEach(workout => {
-            const workoutName = document.createElement('span')
-            const workoutImg = document.createElement('img')
-            workoutName.innerText = workout.name 
-            workoutName.id = workout.id
-            workoutImg.src = workout.image
-
-            form.appendChild(workoutName)
-
-            workoutName.appendChild(workoutImg)
-        
-            workoutName.addEventListener('dblclick', (getWorkoutId))
-            workoutData.appendChild(workoutName)
-            workoutData.appendChild(workoutImg)
-            
-            
-        })
-    };
-
-
-
-    // GET request
-    function getAllWorkouts(){
-        fetch('http://localhost:3000/workouts')
-        .then(resp => resp.json())
-        .then(displayWorkout)
-    }
-    getAllWorkouts();
-    
-    function getWorkoutId(event){
-        fetch(`http://localhost:3000/workouts/${event.target.id}`)
-        .then(resp => resp.json())
-        .then(renderInfo)
-    };
-    
-    function renderInfo(workout){
-        database.innerHTML = ""
-        // const workoutImg = document.createElement('img')
-        const heading = document.createElement('h2')
-        const description = document.createElement('h3')
-        
-        workoutImg.src = workout.image
-        heading.innerText = workout.name
-        description.innerText = workout.description
-    
-        database.append(workoutImg, heading, description)
-    };
-
-    const resetBtn = document.createElement('button')
-    resetBtn.innerText = 'View All Workouts'
-
+    const searchBar = document.getElementById('searchbar')
+    let divCollect = document.querySelector('#database')
     const footer = document.querySelector('footer')
-    footer.appendChild(resetBtn)
 
-    resetBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        handleCardLoad();
-        document.querySelectorAll('.no-match').forEach(e => e.remove())
-      });
+// Renders each card to page
+function renderCards() {
+     fetch('http://localhost:3000/workouts')
+     .then(resp => resp.json())
+     .then(workouts => {
+       workouts.forEach(workout => createCard(workout))})
+     };
+
+    renderCards();
+
+    // Creates cards
+    function createCard(workout){
+       const h4 = document.createElement('h4')
+       const img = document.createElement('img');
+       const btn = document.createElement('button')
+
+       h4.innerText = workout.name 
+
+       img.setAttribute('src', workout.image)
+       img.classList.add('workout-img')
+    
+       btn.setAttribute('id', `${workout.id}`)
+       btn.classList.add('save-btn')
+       btn.innerText = "save";
+    
+    
+    // Append elements to card
+      const divCard = document.createElement('div')
+      divCard.classList.add('card')
+      divCard.append(h4, img, btn)
+      divCollect.append(divCard)
+    
+      
+      divCard.addEventListener("mouseover", () => divCard.setAttribute("style", "box-shadow: 30px 30px 10px #545E63"));
+      divCard.addEventListener("mouseout", () => divCard.setAttribute("style", "box-shadow: 0px 0px"));
+    
+        };
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+document.querySelectorAll('.card').forEach(event => event.remove())
+document.querySelectorAll('.not-found').forEach(event => event.remove())
+
+
+fetch('http://localhost:3000/workouts')
+  .then(resp => resp.json())
+  .then(workouts => {
+    workouts.forEach(workout => {
+        if(workout.muscle === true){
+        createCard(workout)
+    }})
+
+    handleError()},)
+    .catch(error => console.error(error))
+  })
+
+  
+// Creates reset button
+function resetButton(){
+const resetBtn = document.createElement('button')
+resetBtn.innerText = 'View All'
+footer.appendChild(resetBtn)
+
+resetBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    renderCards();
+    document.querySelector('.not-found').forEach(event => event.remove())
+  })
+};
+resetButton()
+
+
 })
